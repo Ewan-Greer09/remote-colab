b2a32fba65c4873bd4b2a61d58fcb5a8416aa75a
Ewan Greer <egreer09@qub.ac.uk>
2024-05-12 01:43:08 +0100

    feat: add basic handler actions and js packages to use in the front end

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,88 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
 	_ "github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+
+	"github.com/Ewan-Greer09/remote-colab/views/index"
 )
+
+type Server struct {
+	*http.Server
+}
+
+type Service struct {
+	Server
+	// db, emailer, etc...
+}
 
 func main() {
 	// ...
+	s := Server{
+		Server: &http.Server{
+			Addr:    ":3000",
+			Handler: NewRouter(),
+		},
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewRouter() http.HandlerFunc {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	//Data routes
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello World - V1 API"))
+		})
+	})
+
+	//Views
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		err := index.Page().Render(context.Background(), w)
+		if err != nil {
+			render.JSON(w, r, fmt.Errorf("there was an issue: %w", err))
+		}
+	})
+
+	r.Get("/index/content", func(w http.ResponseWriter, r *http.Request) {
+		err := index.Content().Render(context.Background(), w)
+		if err != nil {
+			render.JSON(w, r, fmt.Errorf("there was an issue: %w", err))
+		}
+	})
+
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("public/img/collab-logo.png")
+		if err != nil {
+			fmt.Printf("There was an error, %s", err)
+			return
+		}
+		defer f.Close()
+
+		w.Header().Set("Content-Type", "Image/png")
+
+		n, err := io.Copy(w, f)
+		if err != nil {
+			fmt.Printf("error returning favivon: Written (%d)", n)
+			return
+		}
+	})
+
+	return r.ServeHTTP
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Ewan-Greer09/remote-colab
 
 go 1.22.0
 
-require github.com/a-h/templ v0.2.680
+require (
+	github.com/a-h/templ v0.2.680
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/render v1.0.3
+)
+
+require github.com/ajg/form v1.5.1 // indirect
